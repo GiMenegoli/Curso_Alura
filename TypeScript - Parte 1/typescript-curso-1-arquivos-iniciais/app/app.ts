eff4def2dae760bfd6ac2e0660ec0e718beda25e
Giovanna Menegoli <giovanna.menegoli@hotmail.com>
2022-06-30 18:56:05 -0300

    Iniciando curso alura

---
+++ TypeScript - Parte 1/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -0,0 +1,11 @@
+import { Negociacao } from './models/negociacao'
+
+const negociacao = new Negociacao(new Date());
+// console.log(negociacao);
+// negociacao.quantidade = 10000;
+// console.log(negociacao);
+// console.log(negociacao.data);
+// console.log(negociacao.volume);
+// negociacao.quantidad = 10;
+// console.log(negociacao.quantidad)
+
